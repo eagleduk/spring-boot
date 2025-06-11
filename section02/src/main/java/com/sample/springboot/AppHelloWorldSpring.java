c992f64acb2da383ceb26c45d5d8b045a8c948ff
SJ.Lee <eagleduk@gmail.com>
2025-06-11 22:24:31 +0900

    section02 > More use Spring Bean

--- section02/src/main/java/com/sample/springboot/AppHelloWorldSpring.java
+++ section02/src/main/java/com/sample/springboot/AppHelloWorldSpring.java
@@ -2,6 +2,8 @@ package com.sample.springboot;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import java.util.Arrays;
+
 public class AppHelloWorldSpring {
 
     public static void main(String[] args) {
@@ -17,6 +19,14 @@ public class AppHelloWorldSpring {
         System.out.println(context.getBean("name"));
         System.out.println(context.getBean("age"));
         System.out.println(context.getBean("person"));
-        System.out.println(context.getBean("address"));
+        System.out.println(context.getBean("personMethod"));
+        System.out.println(context.getBean("personParameter"));
+        System.out.println(context.getBean("personQualifier"));
+        System.out.println(context.getBean("address2"));
+
+        Arrays.stream(context.getBeanDefinitionNames()).forEach(System.out::println);
+
+        System.out.println(context.getBean(Address.class));
+
     }
 }
--- section02/src/main/java/com/sample/springboot/HelloWorldConfiguration.java
+++ section02/src/main/java/com/sample/springboot/HelloWorldConfiguration.java
@@ -1,14 +1,16 @@
 package com.sample.springboot;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 // Eliminate verbosity in creating Java Beas
 // Public accessor method, constructor,
 // equals, hashcode and toString are automatically created.
 // Released in JDK 16.
 
-record Person(String name, int age) {}
+record Person(String name, int age, Address address) {}
 
 record Address(String firstLine, String city) {}
 
@@ -27,11 +29,32 @@ public class HelloWorldConfiguration {
 
     @Bean
     public Person person() {
-        return new Person("Ronaldo", 40);
+        return new Person("Ronaldo", 40, new Address("Seocho", "Seoul"));
+    }
+
+    @Bean(name = "personMethod")
+    public Person person2() {
+        return new Person(name(), age(), address2());
+    }
+
+    @Bean(name = "personParameter")
+    public Person person3(String name, int age, Address address) {
+        return new Person(name, age, address);
+    }
+
+    @Bean(name = "personQualifier")
+    public Person person4(String name, int age, @Qualifier("addressQualifier") Address address) {
+        return new Person(name, age, address);
     }
 
     @Bean
-    public Address address() {
+    @Primary
+    public Address address2() {
         return new Address("Gangnam", "Seoul");
+    }
+    @Bean
+    @Qualifier("addressQualifier")
+    public Address address3() {
+        return new Address("Songpa", "Seoul");
     }
 }
