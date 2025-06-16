00d8e28d06ea292415ddbdbbec1dff5945ebd231
SJ.Lee <eagleduk@gmail.com>
2025-06-16 22:01:05 +0900

    section03 > dependencies

--- section03/src/main/java/com/sample/springboot/game/GameLauncherApplication.java
+++ section03/src/main/java/com/sample/springboot/game/GameLauncherApplication.java
@@ -1,7 +1,5 @@
-package com.sample.springboot;
+package com.sample.springboot.game;
 
-import com.sample.springboot.game.GameRunner;
-import com.sample.springboot.game.GamingConsole;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
