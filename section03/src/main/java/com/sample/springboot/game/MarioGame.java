864ec5bbbbe283dae3d097d81dd9946324692f1a
SJ.Lee <eagleduk@gmail.com>
2025-06-16 21:08:09 +0900

    section03

---
+++ section03/src/main/java/com/sample/springboot/game/MarioGame.java
@@ -0,0 +1,17 @@
+package com.sample.springboot.game;
+
+public class MarioGame implements GamingConsole {
+
+    public void up() {
+        System.out.println("Jump");
+    }
+    public void down() {
+        System.out.println("Into the hole");
+    }
+    public void right() {
+        System.out.println("Go forward");
+    }
+    public void left() {
+        System.out.println("Go Back");
+    }
+}
