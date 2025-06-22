d85cb2f5bf7fb925f4edfa65fac1a248394283d6
SJ.Lee <eagleduk@gmail.com>
2025-06-22 20:44:02 +0900

    section07 > login form

--- springbootweb/src/main/java/com/udemy/springbootweb/section07/controller/LoginController.java
+++ springbootweb/src/main/java/com/udemy/springbootweb/section07/controller/LoginController.java
@@ -5,17 +5,25 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class LoginController {
 
-    private Logger logger = LoggerFactory.getLogger(getClass());
+    private final Logger logger = LoggerFactory.getLogger(getClass());
 
-    @RequestMapping("/login")
-    public String login(ModelMap model, @RequestParam String name) {
-        model.put("name", name);
-        logger.debug("Login param = {}", name);
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    public String login() {
+        logger.debug("Login");
         return "login";
+    }
+
+    @RequestMapping(value = "/login", method = RequestMethod.POST)
+    public String welcome(ModelMap model, @RequestParam String name, @RequestParam String password) {
+        model.put("name", name);
+        model.put("password", password);
+        logger.debug("name: {}, password: {}", name, password);
+        return "welcome";
     }
 }
--- springbootweb/src/main/resources/META-INF/resources/WEB-INF/jsp/login.jsp
+++ springbootweb/src/main/resources/META-INF/resources/WEB-INF/jsp/login.jsp
@@ -5,6 +5,20 @@
     <title>Title</title>
 </head>
 <body>
-    ${name }, Welcome to login Page!
+    <form method="POST">
+        <div>
+            <label>
+                Name:
+            </label>
+            <input type="text" name="name" />
+        </div>
+        <div>
+            <label>
+                Password:
+            </label>
+            <input type="password" name="password" />
+        </div>
+        <input type="submit" />
+    </form>
 </body>
 </html>
