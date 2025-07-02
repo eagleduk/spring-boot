dfc5496b9ce7256e7757be213c3bb2b3074e1b8b
SJ.Lee <eagleduk@gmail.com>
2025-07-02 15:30:27 +0900

    section08 > messages internationalize

--- springbootweb/src/main/java/com/udemy/springbootweb/section08/controller/HelloWorldController.java
+++ springbootweb/src/main/java/com/udemy/springbootweb/section08/controller/HelloWorldController.java
@@ -1,11 +1,22 @@
 package com.udemy.springbootweb.section08.controller;
 
 import com.udemy.springbootweb.section08.bean.HelloWorldBean;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContext;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Locale;
+
 @RestController
 public class HelloWorldController {
+
+    private MessageSource messageSource;
+
+    public HelloWorldController(MessageSource m) {
+        this.messageSource = m;
+    }
 
     @GetMapping("/hello-world")
     public String helloWorld() {
@@ -15,5 +26,12 @@ public class HelloWorldController {
     @GetMapping("/hello-world-bean")
     public HelloWorldBean helloWorldBean() {
         return new HelloWorldBean("Hello-World");
+    }
+
+    /* Accept-Language */
+    @GetMapping("/hello-world-internationalize")
+    public String helloWorldInternationalize() {
+        Locale locale = LocaleContextHolder.getLocale();
+        return messageSource.getMessage("hello.world.word", null, "Default Message", locale);
     }
 }
