80f906107252a9f1372eae839465d0ffad4f4bda
SJ.Lee <eagleduk@gmail.com>
2025-07-14 21:50:30 +0900

    section15 > Mock test use annotation, parameter

--- springbootweb/src/test/java/com/udemy/springbootweb/section15/MockTest.java
+++ springbootweb/src/test/java/com/udemy/springbootweb/section15/MockTest.java
@@ -2,10 +2,22 @@ package com.udemy.springbootweb.section15;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.List;
+
+@ExtendWith(MockitoExtension.class)
 public class MockTest {
+
+    @Mock
+    DataService dataService;
+
+    @InjectMocks
+    UnitTestLogicServiceImpl u;
 
     @Test
     void getMaxValueTest() {
@@ -18,5 +30,31 @@ public class MockTest {
 
         Assertions.assertEquals(55, result);
 
+    }
+
+    @Test
+    void getMaxValueTest_Annotations() {
+
+        Mockito.when(dataService.dataset()).thenReturn(new int[] { 15, 55, 22, 21});
+        int result = u.getMaxValue();
+
+        Assertions.assertEquals(55, result);
+
+    }
+
+    @Test
+    void listTest() {
+        List<String> list = Mockito.mock(List.class);
+        Mockito.when(list.get(0)).thenReturn("first call").thenReturn("not first call");
+        Assertions.assertEquals("first call", list.get(0));
+        Assertions.assertEquals("not first call", list.get(0));
+    }
+
+    @Test
+    void listParameterTest() {
+        List<String> list = Mockito.mock(List.class);
+        Mockito.when(list.get(Mockito.anyInt())).thenReturn("any index value");
+        Assertions.assertEquals("any index value", list.get(0));
+        Assertions.assertEquals("any index value", list.get(10));
     }
 }
