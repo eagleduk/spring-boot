6130017e978335c789ccd56c7917c8d9dc97d550
SJ.Lee <eagleduk@gmail.com>
2025-07-22 21:58:41 +0900

    section17 > aspect terminology

--- springbootweb/src/main/java/com/udemy/springbootweb/section17/README.md
+++ springbootweb/src/main/java/com/udemy/springbootweb/section17/README.md
@@ -3,3 +3,13 @@
 - 관점지향프로그래밍
 - 공통 관심사를 `aspect`로 구현
 - org.springframework.boot:spring-boot-starter-aop
+
+### Terminology
+| Term       |                                        |
+|:-----------|:---------------------------------------|
+| Advice     | 무엇을 할지. 수행되는 로직                        |
+| Pointcut   | 언제 할지. 수행되려는 시기. 인터셉터 되는 대상이 되는 메소드 명시 |
+| Aspect     | Advice + Pointcut                      |
+| Weaver     | AOP 프레임워크                              |
+| Join Point | 실행 인스턴스                                |
+
--- springbootweb/src/main/java/com/udemy/springbootweb/section17/aop/aspects/LoggingAspect.java
+++ springbootweb/src/main/java/com/udemy/springbootweb/section17/aop/aspects/LoggingAspect.java
@@ -1,23 +1,27 @@
 package com.udemy.springbootweb.section17.aop.aspects;
 
+// Weaver
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+// Aspect - Pointcut + Advice
 @Aspect
 public class LoggingAspect {
 
     private final Logger logger = LoggerFactory.getLogger(getClass());
 
-    // When
+    // Pointcut - When
     // execution(* PACKAGE.*.*(..))
     @Before("execution(* com.udemy.springbootweb.section17.aop.*.*.*(..))")
-    public void logMethodCall(JoinPoint joinPoint) {
-        //What
-        logger.info("Call Method = {}", joinPoint);
+    public void logMethodCall(/* Join Point */JoinPoint joinPoint) {
+
+        // Advice - What
+        logger.info("Call Method = {} needs arguments: {}", joinPoint, joinPoint.getArgs());
     }
 }
